68bcf607f2ec854db33cf0ceea12c60aada7a75c
Alyona Startseva <alyona_startseva.tk@mail.ru>
2021-06-19 21:25:34 +0700

    Feature implemented

---
+++ app.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide')
+
+slides.forEach(item => {
+    item.addEventListener('click', () => {
+        clearActiveClasses()
+        item.classList.add('active')
+    })
+})
+
+function clearActiveClasses() {
+    slides.forEach(item => {
+        item.classList.remove('active')
+    })
+}
